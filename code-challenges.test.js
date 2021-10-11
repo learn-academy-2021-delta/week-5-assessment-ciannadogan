923aea2845d7d2a531ffe403e4235240fc7d8431
ciannadogan <dogan.cianna@yahoo.com>
2021-10-11 08:45:44 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -23,10 +23,55 @@ var secretCodeWord2 = "Gobbledygook"
 var secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe('secretWord', ()=>{
+    it('returns a string with a coded message', ()=>{
+      expect(secretWord(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(secretWord(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(secretWord(SecretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+  
+  // cool got red
 
 
 // b) Create the function that makes the test pass.
 
+// Create a function that takes in a string as an argument
+// declare vowels as a variable
+// input = secretCodeword1, secretCodeWord2, secretCodeWord3
+// use a conditional statement stating if the word includes "a", "e", "i", "o" using .includes()
+// convert a to 4, e to 3, i to 1, and o to 0
+// return output = "L4ck4d41s1c4l", "G0bbl3dyg00k", "3cc3ntr1c" 
+// else return string
+
+
+// const secretWord = (string) => {
+//     newArr = string.split("")
+//     for (let i=0; i<newArr.length; i++){
+//         if (newArr[i].includes("a" || "e" || "i" || "o")){
+//             newArr.push(newArr.replace("a", 4), ("e", 3), ("i", 1) ("o", 0))
+//         }
+//             return newArr.join("")
+//     }
+// }
+// tried to solve it this way^
+
+const secretWord = (string) => {
+    if (string.includes("a") || ("e") || ("i") || ("o")){
+    var newWord = string.replace(/[aA]/g, '4').replace(/[eE]/g, '3').replace(/[iI]/g, '1').replace(/[oO]/g, '0')
+}
+return newWord
+}
+
+// cool got green
+
+// const secretWord = (string) => {
+//    if (string.includes("a", "e", "i", "o")){
+//     return secretWord(vowels)
+//    }else {
+//        return string
+//    }
+// }
 
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
@@ -40,10 +85,33 @@ var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 var letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe('wordsWithAorE', ()=>{
+    it('returns an array of only words that contain a and/or e', ()=>{
+      expect(wordsWithAorE(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+      expect(wordsWithAorE(arrayOfWords2)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
 
+  //cool got red
 
 // b) Create the function that makes the test pass.
 
+// create a function that takes in an array and a string as an argument
+// iterates through the array using filter to remove anything that doesn't contain the value
+// determines which words contain either a or e
+// catch edgecases using or operator and .touppercase 
+// return new array of words that only contain a or e
+
+
+const wordsWithAorE = (array, string) => {
+    let newArr = array.filter(value => {
+        return value.includes(string) ||
+        value.includes(string.toUpperCase())
+    })
+    return newArr
+}
+
+//cool got green
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -57,6 +125,38 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+describe('fullHouse', ()=>{
+    it('returns a boolean value if an array is a full house or not', ()=>{
+      expect(fullHouse(hand1)).toEqual(true)
+      expect(fullHouse(hand2)).toEqual(false)
+      expect(fullHouse(hand3)).toEqual(false)
+    })
+  })
 
+  //cool got red
 
 // b) Create the function that makes the test pass.
+
+//create a function that takes in an array
+//iterate through array to count instances of a number/value
+//use conditional if 3 instances of a number && 2 instances of a number return true
+//else return false
+//got stuck with this one. I wanted to use an increment to count the instances and then use that to
+//create a conditional that would return a boolean value, but after several attempts I couldn't get it. However, I was experimenting with this solution. I did ask for help from fellow classmates that were available (not in 2021d) chat, but those few weren't successful either
+
+    
+    const fullHouse = arr => {
+        const copy = arr.slice();
+        for(let i = 0; i < arr.length; ){
+           const el = copy.splice(i, 1)[0]
+           if(copy.includes()){
+              copy.splice(copy.indexOf(), 1)
+              if(copy.includes()){
+                 return true;
+              }
+           }else{
+              i++
+           }
+        }
+        return false
+     }
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Are (most) Ruby methods accessors or mutators?
 
-  Your answer:
+  Your answer: I just discovered this recently in a review game with my classmates. Most Ruby methods are accessors. 
 
-  Researched answer:
+  Researched answer: I just discovered this recently in a review game with my classmates. Most Ruby methods are accessors. Accessor methods are needed for instance variables. With this, a variable is now referenced as behavior only one time and is no longer referenced directly many times throughout the program.
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A block in Ruby consists of chunks of code. The block is invoked from a function.
 
-  Researched answer:
+  Researched answer: A block in Ruby consists of chunks of code and lies within "do" and "end". The code within the block is not put into effect at the time it is come up against. Ruby remembers where the block appears and then enters the method.
 
 
 
 3. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a package manager that provides distributing of Ruby programs and libraries. I've had experience utilizing this in my most recent challenge questions in class.
 
-  Researched answer:
+  Researched answer: A gems is a package manager for Ruby that provides distributing of programs and libraries. It is a standard set in place to manage the installation of gems, and a server for distributing them.
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: A relational database is a type of database that stores and gives access to data that are related. Each row in the table has its own ID called the key.
 
-  Researched answer:
+  Researched answer: A relational database is a type of database that stores and gives access to data that are related. Each row in the table has its own ID called the key. The columns contain attributes of the data which make it easy to display relationships between the data.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: Primary keys are what provide a unique ID for each line in the data table. It is important because they prevent duplicates from being made in the data. 
 
-  Researched answer:
+  Researched answer: Primary keys are what provide a unique ID for each line in the data table. It is important because they prevent duplicates from being made in the data. Primary keys also have a means of avoiding null values.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A route that provides mapping from HTTP to controller actions Instead of relying on the URL to indicate what site to visit. 
 
-2. JSON:
+2. JSON: JavaScript Object Notation is a text-based format that represents structured data based on JavaScript object syntax. It transmits data in web applications.
 
-3. ERB:
+3. ERB: ERB is a Ruby templating language. ERB files are saved in a plain text format which allows them to be opened in any text editing program.
 
-4. Params:
+4. Params: Params is a variable that is used to pass information between functions or procedures.
 
-5. API:
+5. API: Application Programming Interface, which is a software that allows two applications to talk to each other.
